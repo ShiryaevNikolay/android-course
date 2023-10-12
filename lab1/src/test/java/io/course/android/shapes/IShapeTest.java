8a623d1258f5caf3a89bf6c56c2bf331a10eaa5a
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-10-12 22:00:00 +0300

    Удаляется лишний импорт

--- lab1/src/test/java/io/course/android/shapes/IShapeTest.java
+++ lab1/src/test/java/io/course/android/shapes/IShapeTest.java
@@ -3,8 +3,6 @@ package io.course.android.shapes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class IShapeTest {
 
     @Test
