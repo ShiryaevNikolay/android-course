64aae15d36baf454c5f20037c48fa0cbd7253fcd
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-10-12 21:35:16 +0300

    Сумма площадей фигур

--- lab1/src/main/java/io/course/android/Main.java
+++ lab1/src/main/java/io/course/android/Main.java
@@ -1,7 +1,26 @@
 package io.course.android;
 
+import io.course.android.shapes.Circle;
+import io.course.android.shapes.IShape;
+import io.course.android.shapes.Rectangle;
+import io.course.android.shapes.Triangle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IShape[] shapes = new IShape[]{
+                new Rectangle(10, 20),
+                new Triangle(5, 2, 6),
+                new Circle(14)
+        };
+        double sumArea = getSumArea(shapes);
+        System.out.println("Сумма площадей фигур: " + sumArea);
+    }
+
+    private static double getSumArea(IShape[] shapes) {
+        double sum = 0;
+        for (IShape shape : shapes) {
+            sum += shape.getArea();
+        }
+        return sum;
     }
 }
